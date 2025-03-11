65c40fc00691d20407c6dc2e26317d46a76f81cd
FindlayRoyds <findlayroyds@gmail.com>
2025-03-11 15:38:40 +1300

    finished tutorial

--- wasm-crate/src/lib.rs
+++ wasm-crate/src/lib.rs
@@ -1,3 +1,5 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -47,4 +49,28 @@ pub fn get_people() -> Vec<Person> {
         Person::new("Alice".to_string(), 32),
         Person::new("Bob".to_string(), 117),
     ]
+}
+
+const WASM_MEMORY_BUFFER_SIZE: usize = 2;
+lazy_static! {
+    static ref WASM_MEMORY_BUFFER: Mutex<[u8; WASM_MEMORY_BUFFER_SIZE]> =
+        Mutex::new([0; WASM_MEMORY_BUFFER_SIZE]);
+}
+
+#[wasm_bindgen]
+pub fn store_value_at_idx_zero(value: u8) {
+    let mut buffer = WASM_MEMORY_BUFFER.lock().unwrap();
+    buffer[0] = value;
+}
+
+#[wasm_bindgen]
+pub fn get_wasm_memory_buffer_pointer() -> *const u8 {
+    let buffer = WASM_MEMORY_BUFFER.lock().unwrap();
+    return buffer.as_ptr();
+}
+
+#[wasm_bindgen]
+pub fn read_wasm_memory_buffer_at_idx_one() -> u8 {
+    let buffer = WASM_MEMORY_BUFFER.lock().unwrap();
+    buffer[1]
 }
