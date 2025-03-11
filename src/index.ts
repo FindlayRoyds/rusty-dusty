9f9000cecacef3a724d8cb2fb5513d85f68bea19
FindlayRoyds <findlayroyds@gmail.com>
2025-03-11 15:39:50 +1300

    actually finished tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,42 @@
-import init, { greet, add, get_people, Person } from "../public/wasm/wasm_crate.js"
+import init, {
+  greet,
+  add,
+  get_people,
+  Person,
+  store_value_at_idx_zero,
+  read_wasm_memory_buffer_at_idx_one,
+  get_wasm_memory_buffer_pointer,
+} from "../public/wasm/wasm_crate.js";
 
 async function runWasm() {
-    await init()
-    console.log(greet("Vite Typescript with Wasm"))
+  let rustWasm = await init();
+  console.log(greet("Vite Typescript with Wasm"));
 
-    const addResult = add(12, 18)
-    document.body.textContent = `result: ${addResult}`
+  const addResult = add(12, 18);
+  document.body.textContent = `result: ${addResult}`;
 
-    let people = get_people()
+  let people = get_people();
 
-    console.log(people)
-    people.forEach(person => {
-        console.log(`Name: ${person.name}, Age: ${person.age}`)
-    })
+  console.log(people);
+  people.forEach((person) => {
+    console.log(`Name: ${person.name}, Age: ${person.age}`);
+  });
 
-    let newPerson = new Person("David", 40);
-    people.push(newPerson);
+  let newPerson = new Person("David", 40);
+  people.push(newPerson);
 
-    console.log("After adding a new person")
-    people.forEach(person => {
-        console.log(`Name: ${person.name}, Age: ${person.age}`)
-    })
+  console.log("After adding a new person");
+  people.forEach((person) => {
+    console.log(`Name: ${person.name}, Age: ${person.age}`);
+  });
+
+  store_value_at_idx_zero(24);
+
+  let wasmMemory = new Uint8Array(rustWasm.memory.buffer);
+  let bufferPointer = get_wasm_memory_buffer_pointer();
+  console.log(wasmMemory[bufferPointer + 0]);
+  wasmMemory[bufferPointer + 1] = 10;
+  console.log(read_wasm_memory_buffer_at_idx_one());
 }
 
-runWasm()
+runWasm();
