7d45ba4bdf7489754a262fd855ab71c8be2e172a
FindlayRoyds <findlayroyds@gmail.com>
2025-03-16 13:46:34 +1300

    made test game of life

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,92 @@
 import init, { Grid } from "../public/wasm/wasm_crate.js";
 
+const NUM_CELLS_X = 50;
+const NUM_CELLS_Y = 50;
+
+function testPerformance(name: string, fn: () => void) {
+  const start = performance.now();
+  fn();
+  const end = performance.now();
+  console.log(`${name} took ${end - start} ms`);
+}
+
+function resizeCanvas(canvas: HTMLCanvasElement) {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  const cellSize = Math.min(width / NUM_CELLS_X, height / NUM_CELLS_Y);
+
+  canvas.width = NUM_CELLS_X * cellSize;
+  canvas.height = NUM_CELLS_Y * cellSize;
+
+  return cellSize;
+}
+
+function addClickListener(
+  canvas: HTMLCanvasElement,
+  grid: Grid,
+  pixelSize: number
+) {
+  let isDragging = false;
+  let lastX: number | null = null;
+  let lastY: number | null = null;
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isDragging) {
+      const rect = canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+
+      const cellX = Math.floor(x / pixelSize);
+      const cellY = Math.floor(y / pixelSize);
+      if (cellX == lastX && cellY == lastY) {
+        return;
+      }
+      lastX = cellX;
+      lastY = cellY;
+
+      console.log("clicked");
+      grid.click_at(cellX, cellY);
+      // grid.draw(canvas, pixelSize);
+    }
+  };
+
+  const handleMouseDown = (event: MouseEvent) => {
+    isDragging = true;
+    handleMouseMove(event);
+  };
+
+  const handleMouseUp = () => {
+    isDragging = false;
+    lastX = null;
+    lastY = null;
+  };
+
+  canvas.addEventListener("mousedown", handleMouseDown);
+  canvas.addEventListener("mousemove", handleMouseMove);
+  canvas.addEventListener("mouseup", handleMouseUp);
+  canvas.addEventListener("mouseleave", handleMouseUp);
+}
+
 async function runWasm() {
   let rustWasm = await init();
 
   const canvas = document.getElementById("display_canvas") as HTMLCanvasElement;
-  const game = new Grid(100, 100);
+  const grid = new Grid(NUM_CELLS_X, NUM_CELLS_Y);
+  grid.initialise();
 
-  setInterval(() => {});
+  let pixelSize = resizeCanvas(canvas);
 
-  game.draw_on_canvas(canvas);
+  window.addEventListener("resize", () => {
+    pixelSize = resizeCanvas(canvas);
+  });
+
+  addClickListener(canvas, grid, pixelSize);
+
+  setInterval(() => {
+    testPerformance("updating", () => grid.update());
+    testPerformance("drawing", () => grid.draw(canvas, pixelSize));
+  }, 100);
 }
 
 runWasm();
--- wasm-crate/src/lib.rs
+++ wasm-crate/src/lib.rs
@@ -1,26 +1,31 @@
+use rand::{Rng, rngs::OsRng};
 use wasm_bindgen::prelude::*;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
 #[wasm_bindgen]
 pub struct Grid {
-    width: usize,
-    height: usize,
-    grid: Vec<bool>,
+    width: i32,
+    height: i32,
+    grid: Vec<Vec<bool>>,
+    grid_update: Vec<Vec<bool>>,
+    rng: OsRng,
 }
 
 struct Position {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
 }
 
 #[wasm_bindgen]
 impl Grid {
     #[wasm_bindgen(constructor)]
-    pub fn new(width: usize, height: usize) -> Grid {
+    pub fn new(width: i32, height: i32) -> Grid {
         Grid {
             width,
             height,
-            grid: vec![false; width * height],
+            grid: vec![vec![false; width as usize]; height as usize],
+            grid_update: vec![vec![false; width as usize]; height as usize],
+            rng: OsRng,
         }
     }
 
@@ -34,37 +39,111 @@ impl Grid {
         return positions;
     }
 
-    fn get_cell(&self, x: usize, y: usize) -> bool {
-        let index = y * self.width + x;
-        self.grid[index]
+    fn get_cell(&self, position: &Position) -> Option<bool> {
+        // if position.x < 0 || position.y < 0 || position.x >= self.width || position.y >= self.height
+        // {
+        //     return None;
+        // }
+        let x = (position.x + self.width) % self.width;
+        let y = (position.y + self.height) % self.height;
+        Some(self.grid[y as usize][x as usize])
     }
 
-    fn set_cell(&self, position: Position, value: bool) {
-        let index = position.y + self.width * position.x;
-        self.grid[index] = value;
+    fn set_cell(&mut self, position: &Position, value: bool) {
+        // if position.x < 0 || position.y < 0 || position.x >= self.width || position.y >= self.height
+        // {
+        //     return;
+        // }
+        let x = (position.x + self.width) % self.width;
+        let y = (position.y + self.height) % self.height;
+        self.grid_update[y as usize][x as usize] = value;
+    }
+
+    fn push_update(&mut self) {
+        self.grid = self.grid_update.clone();
     }
 
     #[wasm_bindgen]
-    pub fn update(&self) {
-        for position in self.all_positions() {
-            self.set_cell(x, y, value);
+    pub fn click_at(&mut self, x: i32, y: i32) {
+        for y_offset in -1..=1 {
+            for x_offset in -1..=1 {
+                self.set_cell(
+                    &Position {
+                        x: x_offset + x,
+                        y: y_offset + y,
+                    },
+                    true,
+                );
+            }
         }
+        self.push_update();
     }
 
     #[wasm_bindgen]
-    pub fn draw_on_canvas(&self, canvas: HtmlCanvasElement) -> Result<(), JsValue> {
+    pub fn initialise(&mut self) {
+        for position in self.all_positions() {
+            let value = self.rng.gen_range(0..=1) == 1;
+            self.set_cell(&position, false);
+        }
+        self.push_update();
+    }
+
+    #[wasm_bindgen]
+    pub fn update(&mut self) {
+        for position in self.all_positions() {
+            let mut total = 0;
+            let cell_value = self.get_cell(&position).unwrap_or(false);
+
+            for y in -1..=1 {
+                for x in -1..=1 {
+                    if y == 0 && x == 0 {
+                        continue;
+                    }
+                    let value = self
+                        .get_cell(&Position {
+                            x: position.x + x,
+                            y: position.y + y,
+                        })
+                        .unwrap_or(false);
+                    total += if value { 1 } else { 0 }
+                }
+            }
+            // total += self.rng.gen_range(-4..=5);
+            if cell_value {
+                self.set_cell(&position, total == 2 || total == 3);
+            } else {
+                self.set_cell(&position, total == 3);
+            }
+        }
+        self.push_update();
+    }
+
+    #[wasm_bindgen]
+    pub fn draw(&self, canvas: HtmlCanvasElement, pixel_size: f64) -> Result<(), JsValue> {
         let context = canvas
             .get_context("2d")?
             .ok_or("Failed to get canvas context")?
             .dyn_into::<CanvasRenderingContext2d>()?;
 
+        context.clear_rect(
+            0.0,
+            0.0,
+            pixel_size * self.width as f64,
+            pixel_size * self.height as f64,
+        );
+
         for position in self.all_positions() {
-            context.set_fill_style_str(if self.get_cell(position.x, position.y) {
-                "blue"
+            context.set_fill_style_str(if self.get_cell(&position).unwrap_or(false) {
+                "black"
             } else {
-                "red"
+                "white"
             });
-            context.fill_rect((position.x * 5) as f64, (position.y * 5) as f64, 5.0, 5.0);
+            context.fill_rect(
+                position.x as f64 * pixel_size,
+                position.y as f64 * pixel_size,
+                pixel_size,
+                pixel_size,
+            );
         }
 
         Ok(())
